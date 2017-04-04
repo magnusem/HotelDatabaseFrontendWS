7c495a09da6312c2dbec291492bf73f8d7e2986e
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-04-04 11:32:54 +0200

    Add project files.

---
+++ HotelDatabaseFrontendWS/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace HotelDatabaseFrontendWS.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
